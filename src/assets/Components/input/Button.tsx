a2c4857f71322b68e1c7119189255d1cf78b0e55
leandro almeida da silva <almeidaleandro.silva@gmail.com>
2023-07-18 10:37:23 -0300

    style: add border radius for buttons

--- src/assets/Components/input/Button.tsx
+++ src/assets/Components/input/Button.tsx
@@ -4,6 +4,8 @@ export const ButtonStyled = styled.button<{$primary?: boolean}>`
   background-color: ${ props => props.$primary 
                       ? props.theme.colors.blue
                       : props.theme.colors.green};
+  border: none;
+  border-radius: 5px;
   color: ${ ( { theme } ) => theme.colors.white };  
   padding: 1rem 0;
   text-transform: ${ props => !props.$primary 
